06e3002c2d5f60411a7ea93bd853246823142c9a
AditMeh <aditmeh123@gmail.com>
2023-01-28 02:22:54 -0500

    got rid of model based vis

--- visualization.py
+++ visualization.py
@@ -1,13 +1,14 @@
-import torch
-from ViT import ViT
-from celeba_dataloader import create_dataloaders, create_false_dataloader
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
+import torch
 
+from utils import create_dataloaders
+from ViT import ViT
 
 def visualize_attention_maps(image, model, device):
     logits, att_mat = model(image)
+    print(logits)
     print(torch.sigmoid(logits).item())
     pred = int((torch.sigmoid(logits) > 0.5).detach().cpu().numpy().item())
 
@@ -41,27 +42,6 @@ def visualize_attention_maps(image, model, device):
     image = image.squeeze(0).permute(1, 2, 0).detach().cpu().numpy()
     result = ((mask * image)*255).astype("uint8")
     return result, image, mask, pred
-
-    # f, ax = plt.subplots(1, 3)
-    # ax[2].imshow(mask, cmap='viridis')
-    # ax[1].imshow(result)
-    # ax[0].imshow(image)
-
-    # f, ax = plt.subplots(1, 3)
-    # ax[0].imshow(image)
-    # ax[1].imshow(image)
-    # ax[1].imshow(mask, cmap='viridis', alpha=0.5)
-    # ax[2].imshow(mask)
-    # ax[0].set_xticks([])
-    # ax[0].set_yticks([])
-    # ax[1].set_xticks([])
-    # ax[1].set_yticks([])
-    # ax[2].set_xticks([])
-    # ax[2].set_yticks([])
-    # f.savefig("cmap.png")
-
-    # return result
-
 
 def compare_models(models: list, batch, device):
     results = []
@@ -98,59 +78,45 @@ def compare_models(models: list, batch, device):
 
     f.savefig("cmap.png")
 
+def plot_model_map(model: list, batch, device):
+    results = []
+    out = visualize_attention_maps(
+        batch, model, device)  # result, image, mask, pred
+    results.append(out)
+    f, ax = plt.subplots(1, 3)
+    f.tight_layout()
+    for i, output_visualization in enumerate(results):
+        result, image, mask, pred = output_visualization
+        pred = "Blond" if pred else "Not Blonde"
+        ax[0].imshow(image)
+        ax[1].imshow(image)
+        ax[1].imshow(np.squeeze(mask), cmap='viridis', alpha=0.5)
+        ax[2].imshow(np.squeeze(mask))
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        ax[1].set_xticks([])
+        ax[1].set_yticks([])
+        ax[2].set_xticks([])
+        ax[2].set_yticks([])
+        ax[0].set_title(f"Image", fontsize=8)
+        ax[1].set_title(f"Attention Map", fontsize=8)
+        ax[2].set_title(f"Prediction: {pred}", fontsize=8)
+    f.set_size_inches(18.5, 10.5)
+    f.savefig("cmap.png")
 
-def average_attention_map(batch, model, device):
-    images, _ = batch
-    images = images.to(device)
-    attention_maps = []
-    for image in images:
-        attention_map = visualize_attention_maps(image, model, device)
-        attention_maps.append(attention_map)
-    sum_attention_maps = sum(attention_maps)
-    num_images = len(images)
-    avg_attention_map = sum_attention_maps / num_images
-    return avg_attention_map
-
-
-def visualize_average_attention_map(batch, model, device):
-    avg_attention_map = average_attention_map(batch, model, device)
-    f, ax = plt.subplots(1, 1)
-    ax[0].imshow(avg_attention_map)
-    ax[0].set_xticks([])
-    ax[0].set_yticks([])
-    f.savefig("cmap_average.png")
 
 
 if __name__ == "__main__":
     device = (torch.device('cuda') if torch.cuda.is_available()
               else torch.device('cpu'))
 
-    vit_L = ViT(16, 224, embedding_dim=2048, hidden_dim=1024,
-                num_heads=16, num_layers=6, num_classes=1).to(device=device)
+    nums_heads = [8, 8, 8, 8, 8, 8]
 
-    vit_L.load_state_dict(torch.load("vit.pt"))
-    vit_L.eval()
+    vit = ViT(16, 224, embedding_dim=2048, hidden_dim=1024,
+            nums_heads=nums_heads, num_layers=6, num_classes=1).to(device=device)
 
-    vit_M = ViT(16, 224, embedding_dim=2048, hidden_dim=1024,
-                num_heads=16, num_layers=1, num_classes=1).to(device=device)
+    train, val, _ = create_dataloaders(1, 0.9)
 
-    vit_M.load_state_dict(torch.load("vit_small_more.pt"))
-    vit_M.eval()
+    batch = next(iter(val))[0].to(device=device)
 
-    vit_S = ViT(16, 224, embedding_dim=16, hidden_dim=16,
-                num_heads=16, num_layers=1, num_classes=1).to(device=device)
-
-    vit_S.load_state_dict(torch.load("vit_small_same_heads.pt"))
-    vit_S.eval()
-
-    train, val = create_dataloaders(1, 0.9)
-    fake = create_false_dataloader(1)
-    batch = next(iter(train))[0].to(device=device)
-
-    compare_models([vit_S, vit_M, vit_L], batch, device)
-
-    # print(
-    #     f'prediction: {(torch.sigmoid(out)> 0.5).detach().cpu().numpy().item()}')
-    # result = visualize_attention_maps(batch, vit_S, device)
-
-    # cv2.imwrite("map.png", cv2.cvtColor(result, cv2.COLOR_RGB2BGR))
+    ret = plot_model_map(vit, batch, device)
